81acd747f9b6ca8b41fca965fcb098bd17dee282
eli-fstn <festinelijah@gmail.com>
2025-03-10 01:04:07 +0800

    Add files via upload

---
+++ Calculator-simple/calcFunction.js
@@ -0,0 +1,32 @@
+let displayValue;
+const display = document.getElementById("output");
+
+function updateDisplay() {
+    display.textContent = displayValue;
+}
+
+function appendToDisplay(value) {
+    if (displayValue === '0' && value !== '.') {
+        displayValue = value;
+    } else {
+        displayValue += value;
+    }
+    updateDisplay();
+}
+
+function clearDisplay() {
+    displayValue = ' ';
+    updateDisplay();
+}
+
+function backspace() {
+    if (displayValue.length > 1) {
+        displayValue = displayValue.slice(0, -1);
+    } else {
+        displayValue = '0';
+    }
+    updateDisplay();
+}
+function calculate() {
+    document.getElementById("output").textContent = "I miss you";
+}
